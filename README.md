7f002fe73fdc85c8c237d5ead0f330fe7f05e30c
PanaW <pana.wang@outlook.com>
2022-05-30 09:35:18 +0800

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# hardhat-conflux-example
+
+First install the `hardhat-conflux` plugin and `js-conflux-sdk@next`.
+
+Then check `hardhat.config.js` and `scripts/sample-conflux.js` for example.
---
+++ scripts/sample-conflux.js
@@ -0,0 +1,38 @@
+const hre = require("hardhat");
+const {
+  ConfluxSDK,  // The js-conflux-sdk SDK
+  conflux,    // The Conflux instance
+} = hre;
+
+async function main() {
+  const accounts = await conflux.getSigners();
+
+  const epochNumber = await conflux.cfx.epochNumber();
+  console.log('Current epochNumber: ', epochNumber);
+
+  const balance = await conflux.cfx.getBalance(accounts[0].address);
+  console.log(`Balance of ${accounts[0].address} :`, ConfluxSDK.Drip(balance).toCFX());
+
+  // We get the contract to deploy
+  /* const Greeter = await conflux.getContractFactory("Greeter");
+  
+  const deployReceipt = await Greeter.constructor("Hello, Hardhat!").sendTransaction({
+    from: accounts[0].address,
+  }).executed();
+
+  console.log("Greeter deployed to:", deployReceipt.contractCreated); */
+
+  // Interact with the contract
+  const contractAddress = 'cfxtest:acdes0ybnwh7uu0w8szcvpx01ehaswc6py7vm4f1yk';
+  // We got a js-conflux-sdk contract instance
+  const greeter = await conflux.getContractAt('Greeter', contractAddress);
+  const greeting = await greeter.greet();
+  console.log(greeting);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
